5ae7980d71f85e1b97345cfe485851a320518b72
Kevin Lopez <kev@mac.home>
2025-02-11 18:46:08 +0100

    Disable deprected plugin null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,6 +1,7 @@
 return {
     'jose-elias-alvarez/null-ls.nvim',
     lazy = false,
+    enabled = false,
     config = function ()
         local null_ls = require("null-ls")
         null_ls.setup({
