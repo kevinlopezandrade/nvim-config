2964927785c39c09fb03bfe6b0a1c721fa2fa67e
Kev Lopez <kevin@tufalabs.ai>
2026-02-12 18:04:51 +0000

    Update config

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,12 +1,17 @@
 return {
     {
         "ellisonleao/gruvbox.nvim",
+        enabled = false,
     },
+    {
     "rebelot/kanagawa.nvim",
+        enabled= false
+    },
     {
         "EdenEast/nightfox.nvim",
         priority = 1000,
         lazy = false,
+        enabled = True,
         config = function()
             vim.cmd("colorscheme duskfox")
         end
@@ -19,5 +24,19 @@ return {
             -- tundra.setup({})
             -- vim.cmd("colorscheme tundra")
         end
+    },
+
+    {
+        "neovim-idea/catppuccin-reloaded-nvim",
+        dependencies = { "catppuccin/nvim" },
+        enabled = false,
+        priority = 500,
+        config = function()
+            require("catppuccin-reloaded").setup({
+                -- here you can insert your usual catppuccin options
+                catppuccin = {}
+            })
+        end,
     }
+
 }
